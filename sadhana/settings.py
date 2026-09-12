7297c2056a077a67288fcdc37f142ea82ccacc9d
jaychandra <jaychandradasbapu05@gmail.com>
2026-09-12 23:39:53 +0530

    fix: reduce conn_max_age and add health checks to handle pooler timeouts

--- sadhana/settings.py
+++ sadhana/settings.py
@@ -122,8 +122,9 @@ if _db_url:
     import dj_database_url
     DATABASES["default"] = dj_database_url.parse(
         _db_url,
-        conn_max_age=600,
+        conn_max_age=300,
         ssl_require=True,
+        conn_health_checks=True,
     )
 
 
