cb4d7f0e90032e42773790ed7538163885f62c4c
jaychandra <jaychandradasbapu05@gmail.com>
2026-09-12 23:06:39 +0530

    fix: remove migrate from build.sh to avoid pooler connection limit

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ pip install -r requirements.txt
 # Collect static files for WhiteNoise
 python manage.py collectstatic --noinput
 
-# Run database migrations (DATABASE_URL must be set in Render env vars)
-python manage.py migrate --noinput
+# Run database migrations manually via Render Shell after first deploy:
+# python manage.py migrate --noinput
